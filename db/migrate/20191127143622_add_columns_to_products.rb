638f3a3e97c668e31790840ae5dc07e917004e2b
Will Hodgson <will.hodgson@current-rms.com>
2019-11-27 14:45:44 +0000

    Automatically backed up by Learn

--- db/migrate/20191127143622_add_columns_to_products.rb
+++ db/migrate/20191127143622_add_columns_to_products.rb
@@ -1,5 +1,6 @@
 class AddColumnsToProducts < ActiveRecord::Migration[5.0]
   def change
-    add_column 
+    add_column :products, :description, :string
+    add_column :products, :inventory, :integer
   end
 end
