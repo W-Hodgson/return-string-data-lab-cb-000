1d9467fbc2570fdd6a8fa5826fb687ab06cacb79
Will Hodgson <will.hodgson@current-rms.com>
2019-11-27 15:30:08 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :orders
   resources :invoices
   resources :products
-  post "products/:id/description", to: "products#description"
+  get "products/:id/description", to: "products#description"
 end
